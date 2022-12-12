e69d2cc856949c70661d8e791443c9ddb86dcacd
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-12 12:17:27 +0530

    Updates to login route handling

--- index.js
+++ index.js
@@ -1,68 +1,81 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
 const passport = require('passport');
-const session = require('express-session')
+const session = require('express-session');
 const JwtStrategy = require('passport-jwt').Strategy;
-const app = express();
 const PORT = 3000;
-const jwtSecret = 'secret-key'
+const jwtSecret = 'secret-key';
 // Set up a temporary store for user data
 // Later we'll hook it up to db
 const users = new Map();
 
-const jwtStrategy = new JwtStrategy({
- jwtFromRequest : req => req.headers.authorization,
- secretOrKey : 'jwt-secret'},
-(jwtPaylod, done) => {
-  return done( null,
-    {
-      email : jwtPaylod.email,
-      name :  jwtPaylod.name,
-    })
-}
-)
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(session({
+  secret : 'session-secret',
+  resave : false,
+  saveUninitialized : false
+}))
+
+const jwtStrategy = new JwtStrategy(
+  {
+    jwtFromRequest: (req) => req.headers.authorization,
+    secretOrKey: 'jwt-secret',
+  },
+  (jwtPaylod, done) => {
+    return done(null, {
+      email: jwtPaylod.email,
+      name: jwtPaylod.name,
+    });
+  }
+);
 passport.use(jwtStrategy);
 
-function getUserDetails(email){
+app.use(passport.initialize());
+app.use(passport.session());
+
+function getUserDetails(email) {
   return users.get(email);
 }
 
-function credentialsValid( email, password){
-  if ( !users.has(email)){
+function credentialsValid(email, password, res) {
+  if (!users.has(email)) {
     res.status(401).send(`Email ${email} is not registered`);
-    return false;
+    return;
   }
   const userdetails = getUserDetails(email);
   return bcrypt.compareSync(password, userdetails.password);
 }
 
-passport.serializeUser((user,done) => {
+passport.serializeUser((user, done) => {
   done(null, user.email);
 });
 
-passport.deserializeUser((email,done)=>{
-  return done( null, getUserDetails(email));
+passport.deserializeUser((email, done) => {
+  return done(null, getUserDetails(email));
 });
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.get("/login", (req,res)=>{
-  if ( credentialsValid( req.body.email, req.body.password)){
+app.get('/login', (req, res) => {
+  if (credentialsValid(req.body.email, req.body.password, res)) {
     // Create and provide jwt to user
-  const jwt = jwt.sign({
-    email : req.body.email
-  }, jwtSecret);
+    const jwt = jwt.sign(
+      {
+        email: req.body.email,
+      },
+      jwtSecret
+    );
 
-  return res.json({jwt});
+    return res.json({ jwt });
   }
 
-  res.status(401).send("Invalid Credentials");
-})
+  res.status(401).send('Invalid Credentials');
+});
 
 // Use Passport for authentication
-app.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-  // If Authentication is successful we will have access to
-  // req.user object
+app.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // If Authentication is successful, content from Jwt claims will be 
+  // available in req.user property
   res.send(`Hi ${req.user.email}`);
 });
 
