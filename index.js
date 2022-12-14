7962f352e3622ba1af227cb437e5a93ea2975a28
Rajesh <rajeshceg3@gmail.com>
2022-12-14 05:31:39 +0000

    JWT updates

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const passport = require('passport');
 const session = require('express-session');
 const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const bcrypt = require('bcryptjs');
 const PORT = 3000;
 const jwtSecret = 'secret-key';
@@ -19,29 +20,30 @@ app.use(session({
   saveUninitialized : false
 }))
 
-const jwtStrategy = new JwtStrategy(
-  {
-    jwtFromRequest: (req) => req.headers.authorization,
-    secretOrKey: jwtSecret,
-  },
-  (jwtPaylod, done) => {
+const jwtOptions =  {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: jwtSecret,
+};
+
+const jwtStrategy = new JwtStrategy( jwtOptions,
+  (jwtPayload, done) => {
     return done(null, {
-      email: jwtPaylod.email,
-      name: jwtPaylod.name,
+      username: jwtPayload.username
     });
   }
 );
+
 passport.use(jwtStrategy);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-  done(null, user.email);
+  done(null, user.username);
 });
 
-passport.deserializeUser((email, done) => {
-  return done(null, getUserDetails(email));
+passport.deserializeUser((username, done) => {
+  return done(null, getUserDetails(username));
 });
 
 // Collect username and password and validate it
@@ -119,7 +121,7 @@ app.post('/loginAPI',(req,res)=>{
   }
   else{
     const token = jwt.sign({username:getUserDetails(req.body.username).username}, jwtSecret);
-    req.session.token = token;
+
     res.json({
       success: true,
       token : token
@@ -128,12 +130,13 @@ app.post('/loginAPI',(req,res)=>{
   })
 
 
-// Use Passport for authentication
+// Use Passport for authentication to a protected route
 app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
   // If Authentication is successful, content from Jwt claims will be 
   // available in req.user property
-  console.log("Received correct token, allowing user to the route")
+  console.log("Received correct token")
   res.send(`Hi ${req.user.username}`);
+
 });
 
 app.listen(PORT, () => {
